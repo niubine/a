f09f2b1f6599cd5e91d6ae4dfc1293f12d43232d
niubine <jackydoys@outlook.com>
2026-01-14 01:03:31 +0800

    Fix missing clickable import

--- main/src/main/java/tw/firemaples/onscreenocr/floatings/compose/mainbar/MainBarContent.kt
+++ main/src/main/java/tw/firemaples/onscreenocr/floatings/compose/mainbar/MainBarContent.kt
@@ -5,6 +5,7 @@ import android.graphics.Point
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
